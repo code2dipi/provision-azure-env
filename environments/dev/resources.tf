1c1e17e2ee2841ed9d129c2a47a526f335ac09f3
Dipi <dipineupane@gmail.com>
2025-06-29 23:32:35 +0200

    add vnet subnets and postgres server

--- environments/dev/resources.tf
+++ environments/dev/resources.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "myrg" {
-    name     = var.resource_group_name
-    location = var.location
-  
+  name     = var.resource_group_name
+  location = var.location
+
 }
 resource "azurerm_virtual_network" "myvnet" {
   name                = "vnet-terraform-dev"
@@ -9,9 +9,63 @@ resource "azurerm_virtual_network" "myvnet" {
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 }
-resource "azurerm_subnet" "mysubnet" {
-  name                 = "subnet-terraform-dev"
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "Subnet-App"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "Subnet-Database"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_subnet" "keycloak_subnet" {
+  name                 = "Subnet-Keycloak"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+# private endpoint for postgres
+resource "azurerm_postgresql_flexible_server" "pg" {
+  name                   = "postgres-db-server"
+  resource_group_name    = azurerm_resource_group.myrg.name
+  location               = azurerm_resource_group.myrg.location
+  version                = "14"
+
+  delegated_subnet_id    = azurerm_subnet.db_subnet.id
+
+  administrator_login    = var.postgres_admin_username
+  administrator_password = var.postgres_admin_password
+
+  sku_name               = "B_Standard_B1ms"
+  storage_mb             = 32768
+  backup_retention_days  = 7
+  geo_redundant_backup_enabled = false
+
+
+}
+
+resource "azurerm_postgresql_flexible_server_database" "testdb" {
+  name      = "testdb"
+  server_id = azurerm_postgresql_flexible_server.pg.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}
+
+resource "azurerm_private_endpoint" "pg_private_endpoint" {
+  name                = "pg-private-endpoint"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  subnet_id           = azurerm_subnet.db_subnet.id
+
+  private_service_connection {
+    name                           = "pg-psc"
+    private_connection_resource_id =  azurerm_postgresql_flexible_server.pg.id
+    subresource_names              = ["postgresqlServer"]
+    is_manual_connection           = false
+  }
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,11 +1,20 @@
 
 variable "resource_group_name" {
-    description = "Resource group name"
-    default = "rg-terraform-dev"
-    type  =string
+  description = "Resource group name"
+  default     = "rg-terraform-dev"
+  type        = string
 }
 variable "location" {
-    description = "Azure region for resources"
-    type = string
-    default = "westeurope"
+  description = "Azure region for resources"
+  type        = string
+  default     = "westeurope"
+}
+variable "postgres_admin_username" {
+  description = "PostgreSQL admin username"
+  type        = string
+}
+variable "postgres_admin_password" {
+  description = "PostgreSQL admin password"
+  type        = string
+  sensitive   = true
 }
